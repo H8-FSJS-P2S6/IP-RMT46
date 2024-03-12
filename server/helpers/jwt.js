e31b7ba737d5e938feae7c3f4c567b24f50439d2
Hackim18 <khakim668@gmail.com>
2024-03-12 23:45:49 +0700

    feat: add jsonwebtoken on the server

---
+++ server/helpers/jwt.js
@@ -0,0 +1,6 @@
+const { sign, verify } = require("jsonwebtoken");
+const secret = "rahasia";
+module.exports = {
+  signToken: (payload) => sign(payload, secret),
+  verifyToken: (token) => verify(token, secret),
+};
