97df09406e5516686d74db434a7f858f83b98a03
arief-rahman-rizaldhi <ariefhiruma@gmail.com>
2024-03-15 11:30:09 +0700

    feat:for deploy

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 
 const express = require('express');
 const router = require("./routers");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
