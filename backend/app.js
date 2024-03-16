300e52b7165be20557c363accb2e9486ef12ea69
Syarah Namira <syarahnamira@gmail.com>
2024-03-17 00:23:47 +0700

    use PORT environment variable

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 var cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors())
